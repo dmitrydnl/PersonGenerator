b309e56e792fa89429b15211949460a308402d20
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2020-02-03 19:00:36 +0300

    Create NumberRandom

---
+++ src/PersonGenerator/Random/NumberRandom.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonGenerator.Random
+{
+    internal static class NumberRandom
+    {
+        private readonly static System.Random random = new System.Random();
+
+        internal static int GetRandomNumber(int min, int max)
+        {
+            max = max != int.MaxValue ? max + 1 : max;
+            return random.Next(min, max);
+        }
+    }
+}
