4fa41239a637348644593141969923c941b34a26
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2020-02-03 20:07:01 +0300

    Create DateRandom

---
+++ src/PersonGenerator/Random/DateRandom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PersonGenerator.Random
+{
+    internal static class DateRandom
+    {
+        internal static DateTime GetRandomDayAndMonth()
+        {
+            DateTime date = new DateTime();
+            int days = NumberRandom.GetRandomNumber(0, 364);
+            date = date.AddDays(days);
+            return date;
+        }
+    }
+}
