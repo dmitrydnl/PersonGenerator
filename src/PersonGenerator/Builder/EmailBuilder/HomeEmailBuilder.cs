7e7d05a8f75b5af89d10fd7de6585d57da9cb483
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2020-02-10 15:10:29 +0300

    Add alternative case in build email

--- src/PersonGenerator/Builder/EmailBuilder/HomeEmailBuilder.cs
+++ src/PersonGenerator/Builder/EmailBuilder/HomeEmailBuilder.cs
@@ -15,7 +15,13 @@ namespace PersonGenerator.Builder.EmailBuilder
             string firstName = list.Length >= 1 ? list[0] : null;
             string lastName = list.Length >= 2 ? list[1] : null;
             string age = list.Length >= 3 ? list[2] : null;
-            return firstName + lastName + age + "@gmail.com";
+
+            if (!string.IsNullOrEmpty(firstName) || !string.IsNullOrEmpty(lastName))
+            {
+                return firstName + lastName + age + "@gmail.com";
+            }
+
+            return Build();
         }
     }
 }
