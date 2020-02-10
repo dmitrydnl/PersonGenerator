384b88aaa50e3dd89b8649341e3f73b280379547
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2020-02-10 14:46:41 +0300

    Update Person class

--- src/PersonGenerator/Person.cs
+++ src/PersonGenerator/Person.cs
@@ -5,12 +5,17 @@ namespace PersonGenerator
 {
     public class Person
     {
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; internal set; }
+        public string MiddleName { get; internal set; }
+        public string LastName { get; internal set; }
+        public int Age { get; internal set; }
+        public DateTime BirthDate { get; internal set; }
+        public string Email { get; internal set; }
+
+        internal Person()
+        {
+
+        }
 
         public void Print()
         {
